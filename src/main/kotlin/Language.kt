cbd8314ff6865650c93e3ea68bf244d0fd44e556
Corentin <corentin.bruneau@tchek.ai>
2022-02-17 11:57:37 +0100

    [ADD] Languages: Handle german column

--- src/main/kotlin/Language.kt
+++ src/main/kotlin/Language.kt
@@ -1,4 +1,5 @@
 enum class Language(val columnName: String, val code: String) {
     FR(columnName = "French (France), fr-FR", code = "fr"),
     EN(columnName = "English (UK), en-EN", code = "en"),
+    DE(columnName = "Deutsch (DE), de-DE", code = "de"),
 }
