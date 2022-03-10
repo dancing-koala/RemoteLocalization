83184d1fadab23195c5e172dc38216d50ffc81b2
Corentin <corentin.bruneau@tchek.ai>
2022-03-10 14:29:24 +0100

    [ADD] Languages: Add italian language

--- src/main/kotlin/Language.kt
+++ src/main/kotlin/Language.kt
@@ -2,4 +2,5 @@ enum class Language(val columnName: String, val code: String) {
     FR(columnName = "French (France), fr-FR", code = "fr"),
     EN(columnName = "English (UK), en-EN", code = "en"),
     DE(columnName = "Deutsch (DE), de-DE", code = "de"),
+    IT(columnName = "Italiano (IT), it-IT", code = "it"),
 }
